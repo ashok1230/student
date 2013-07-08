71a8e640eaa9c82858eb5b74c1a122984c20c56f
ashok1230 <gadiashok0410@gmail.com>
2013-07-08 09:39:33 +0530

    Update manage.py

--- manage.py
+++ manage.py
@@ -37,3 +37,58 @@ import settings
 
 if __name__ == "__main__":
     execute_manager(settings)
+    
+    
+routers
+
+"""
+NewWebsite.com's router
+Establishes rules to forward user and session db requests 
+to ExampleA.com unless we are accessing the admin.
+"""
+import threading
+
+# Object to hold request data
+request_cfg = threading.local()
+
+class RouterMiddleware(object):
+    """
+    Sets a flag if we are accessing Django admin to 
+    prevent database rerouting for the auth model.  
+    Removes the flag once the request has been processed.
+    """
+
+    def process_view(self, request, view_func, args, kwargs):
+        if request.path.startswith('/admin'):
+            request_cfg.admin = True
+
+    def process_response(self, request, response):
+        if hasattr(request_cfg, 'admin'):
+            del request_cfg.admin
+    return response
+
+class UserSessionRouter(object):
+    """
+    Redirects database IO for the auth and sessions 
+    models to OldWebsite.com.
+    """
+
+    def db_for_read(self, model, **hints):
+        if not hasattr(request_cfg, 'admin'):
+            if model._meta.app_label == 'auth':
+                return 'usersandsessions'
+            elif model._meta.app_label == 'accounts':
+                return 'usersandsessions'
+            elif model._meta.app_label == 'sessions':
+                return 'usersandsessions'
+        return None
+
+    def db_for_write(self, model, **hints):
+        if not hasattr(request_cfg, 'admin'):
+            if model._meta.app_label == 'auth':
+                return 'usersandsessions'
+            elif model._meta.app_label == 'accounts':
+                return 'usersandsessions'
+            elif model._meta.app_label == 'sessions':
+                return 'usersandsessions'
+        return None
